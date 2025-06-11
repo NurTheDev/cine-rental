43a57145f9a3e3c1fbe7a1b6daabed601b3f8c26
NurTheDev <nur756.islam@gmail.com>
2025-06-11 18:24:38 +0600

    added header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,8 +1,11 @@
 import Logo from "../assets/Logo.jsx";
+import Ring from "../assets/Ring.jsx";
+import Moon from "../assets/icons/Moon.jsx";
+import Shopping from "../assets/Shopping.jsx";
 export default function Header() {
     return (
         <header>
-            <nav className="container flex items-center justify-between space-x-10 py-6">
+            <nav className="container  flex items-center justify-between space-x-10 py-6">
                 <a href="index.html">
                     <Logo className="w-32 h-10"  />
                 </a>
@@ -11,19 +14,19 @@ export default function Header() {
                     <li>
                         <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                            href="#">
-                            <img src="./assets/ring.svg" width="24" height="24" alt=""/>
+                            <Ring className="w-6 h-6"/>
                         </a>
                     </li>
                     <li>
                         <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                            href="#">
-                            <img src="./assets/icons/moon.svg" width="24" height="24" alt=""/>
+                            <Moon className="w-6 h-6"/>
                         </a>
                     </li>
                     <li>
                         <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                            href="#">
-                            <img src="./assets/shopping-cart.svg" width="24" height="24" alt=""/>
+                            <Shopping className="w-6 h-6"/>
                         </a>
                     </li>
                 </ul>
