04350c5a8a9199f3f5c456ec8286c2d95cebe70f
NurTheDev <nur756.islam@gmail.com>
2025-06-11 12:46:50 +0600

    project initialize

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="dark:bg-body bg-white font-[Sora] dark:text-white text-dark">
+
+        </div>
+    );
+};
+
+export default App;
