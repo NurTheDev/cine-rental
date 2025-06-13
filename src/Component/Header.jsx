2c3df67ac0f605197f0c6c815a57c64d9c48049d
NurTheDev <nur756.islam@gmail.com>
2025-06-13 11:55:28 +0600

    added header.jsx

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -1,32 +1,33 @@
-import Logo from "../assets/Logo.jsx";
-import Ring from "../assets/Ring.jsx";
-import Moon from "../assets/icons/Moon.jsx";
-import Shopping from "../assets/Shopping.jsx";
+import React from "react";
+import logo from "../assets/logo.svg";
+import ring from "../assets/ring.svg";
+import sun from "../assets/icons/sun.svg";
+import shoppingCart from "../assets/shopping-cart.svg";
 export default function Header() {
     return (
         <header>
-            <nav className="container  flex items-center justify-between space-x-10 py-6">
+            <nav className="container flex items-center justify-between space-x-10 py-6">
                 <a href="index.html">
-                    <Logo className="w-32 h-10"  />
+                    <img src={logo || ""} width="139" height="26" alt=""/>
                 </a>
 
                 <ul className="flex items-center space-x-5">
                     <li>
                         <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                            href="#">
-                            <Ring className="w-6 h-6"/>
+                            <img src={ring || ""} width="24" height="24" alt=""/>
                         </a>
                     </li>
                     <li>
                         <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                            href="#">
-                            <Moon className="w-6 h-6"/>
+                            <img src={sun || ""} width="24" height="24" alt=""/>
                         </a>
                     </li>
                     <li>
                         <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
                            href="#">
-                            <Shopping className="w-6 h-6"/>
+                            <img src={shoppingCart || ""} width="24" height="24" alt=""/>
                         </a>
                     </li>
                 </ul>
